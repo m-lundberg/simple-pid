e4c24a5448629b6e0121ae23a2b48f1daea5c13a
Martin <martin95lundberg@gmail.com>
2018-10-09 01:22:07 +0200

    Fix format for params in docstring

--- simple_pid/PID.py
+++ simple_pid/PID.py
@@ -39,17 +39,18 @@ class PID(object):
         :param Kd: The value for the derivative gain Kd
         :param setpoint: The initial setpoint that the PID will try to achieve
         :param sample_time: The time in seconds which the controller should wait before generating a new output value.
-        The PID works best when it is constantly called (eg. during a loop), but with a sample time set so that the
-        time difference between each update is (close to) constant. If set to None, the PID will compute a new output
-        value every time it is called.
-        :param output_limits: The initial output limits to use, given as an iterable with 2 elements, for
-        example: (lower, upper). The output will never go below the lower limit or above the upper limit. Either of
-        the limits can also be set to None to have no limit in that direction. Setting output limits also avoids
-        integral windup, since the integral term will never be allowed to grow outside of the limits.
+                            The PID works best when it is constantly called (eg. during a loop), but with a sample
+                            time set so that the time difference between each update is (close to) constant. If set to
+                            None, the PID will compute a new output value every time it is called.
+        :param output_limits: The initial output limits to use, given as an iterable with 2 elements, for example:
+                              (lower, upper). The output will never go below the lower limit or above the upper limit.
+                              Either of the limits can also be set to None to have no limit in that direction. Setting
+                              output limits also avoids integral windup, since the integral term will never be allowed
+                              to grow outside of the limits.
         :param auto_mode: Whether the controller should be enabled (in auto mode) or not (in manual mode)
         :param proportional_on_measurement: Whether the proportional term should be calculated on the input directly
-        rather than on the error (which is the traditional way). Using proportional-on-measurement avoids overshoot
-        for some types of systems.
+                                            rather than on the error (which is the traditional way). Using
+                                            proportional-on-measurement avoids overshoot for some types of systems.
         """
         self.Kp, self.Ki, self.Kd = Kp, Ki, Kd
         self.setpoint = setpoint
