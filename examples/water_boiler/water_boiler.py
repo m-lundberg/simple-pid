b7751d5926e130ad3e42f79e483e0ac625d70541
Martin <martin95lundberg@gmail.com>
2020-03-05 22:47:19 +0100

    Use Black code style in examples as well.

--- examples/water_boiler/water_boiler.py
+++ examples/water_boiler/water_boiler.py
@@ -10,16 +10,17 @@ class WaterBoiler:
     Simple simulation of a water boiler which can heat up water
     and where the heat dissipates slowly over time
     """
+
     def __init__(self):
         self.water_temp = 20
 
     def update(self, boiler_power, dt):
         if boiler_power > 0:
             # boiler can only produce heat, not cold
-            self.water_temp += 1*boiler_power*dt
+            self.water_temp += 1 * boiler_power * dt
 
         # some heat dissipation
-        self.water_temp -= 0.02*dt
+        self.water_temp -= 0.02 * dt
         return self.water_temp
 
 
@@ -43,7 +44,7 @@ if __name__ == '__main__':
         power = pid(water_temp)
         water_temp = boiler.update(power, dt)
 
-        x += [current_time-start_time]
+        x += [current_time - start_time]
         y += [water_temp]
         setpoint += [pid.setpoint]
 
