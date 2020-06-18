a34e03ca667e87d21018a9a60c796bae84fb4f21
Supegg <supegg.rao@gmail.com>
2020-06-18 10:32:39 +0800

    add a random CrazyBoiler

--- examples/water_boiler/water_boiler.py
+++ examples/water_boiler/water_boiler.py
@@ -3,7 +3,7 @@
 import time
 import matplotlib.pyplot as plt
 from simple_pid import PID
-
+import random
 
 class WaterBoiler:
     """
@@ -23,12 +23,31 @@ class WaterBoiler:
         self.water_temp -= 0.02 * dt
         return self.water_temp
 
+class CrazyBoiler:
+    """
+    it randoms me CRAZY
+    """
+
+    def __init__(self):
+        self.water_temp = 20
+
+    def update(self, boiler_power, dt):
+        if boiler_power > 0:
+            # boiler can only produce heat, not cold
+            self.water_temp += 1 * boiler_power * dt
+
+        # some heat dissipation
+        self.water_temp -= self.water_temp * dt * random.random()
+        return self.water_temp
+
 
 if __name__ == '__main__':
     boiler = WaterBoiler()
+    # boiler = CrazyBoiler()
     water_temp = boiler.water_temp
 
     pid = PID(5, 0.01, 0.1, setpoint=water_temp)
+    # pid = PID(20, 0.9, 1.9, setpoint=water_temp)
     pid.output_limits = (0, 100)
 
     start_time = time.time()
@@ -44,15 +63,19 @@ if __name__ == '__main__':
         power = pid(water_temp)
         water_temp = boiler.update(power, dt)
 
-        x += [current_time - start_time]
-        y += [water_temp]
+        x += [round(current_time - start_time, 3)]
+        y += [round(water_temp, 2)]
         setpoint += [pid.setpoint]
 
         if current_time - start_time > 1:
             pid.setpoint = 100
 
         last_time = current_time
+        time.sleep(0.1)
+        # time.sleep(0.01)
 
+    # print(f"{list(zip(x, y))}")
+    print(y)
     plt.plot(x, y, label='measured')
     plt.plot(x, setpoint, label='target')
     plt.xlabel('time')
