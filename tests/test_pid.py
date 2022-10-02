b5678668f5b5438b7a422044f11c041eb18bc2a7
Martin Lundberg <martin95lundberg@gmail.com>
2022-10-02 14:03:10 +0200

    Add test for time_fn #67

--- tests/test_pid.py
+++ tests/test_pid.py
@@ -106,14 +106,26 @@ def test_sample_time():
     assert pid(100) == control
 
 
-def test_monotonic():
-    # Default time function should be time.monotonic, or time.time in older versions of Python
+def test_time_fn():
     pid = PID()
 
+    # Default time function should be time.monotonic, or time.time in older versions of Python
     if sys.version_info < (3, 3):
         assert pid.time_fn == time.time
     else:
         assert pid.time_fn == time.monotonic
+
+    i = 0
+    def time_function():
+        nonlocal i
+        i += 1
+        return i
+    pid.time_fn = time_function
+
+    for j in range(1, 5):
+        # Call pid a few times and verify that the time function above was used
+        pid(0)
+        assert pid._last_time == j
 
 
 def test_auto_mode():
